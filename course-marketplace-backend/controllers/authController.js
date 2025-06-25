f97dcfd8b5e9adc7a621db15007cbb6cfddc1077
divyanshu02raj <divyanshuraj2206@gmail.com>
2025-06-25 13:06:53 +0530

    m

--- course-marketplace-backend/controllers/authController.js
+++ course-marketplace-backend/controllers/authController.js
@@ -7,12 +7,7 @@ const generateToken = (user) =>
   jwt.sign({ id: user._id, name: user.name , role: user.role }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
-  res.cookie("token", token, {
-  httpOnly: true,
-  sameSite: "Lax",
-  secure: process.env.NODE_ENV === "production",
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-});
+
 
 res.json({ user });
 
