987e341735a35643dc42d5f7cd22c7016a6b3c1f
divyanshu02raj <divyanshuraj2206@gmail.com>
2025-07-05 02:17:50 +0530

    Course creation error fixed

--- course-marketplace-frontend/src/InstructorDashboardComponents/CreateCourseView.jsx
+++ course-marketplace-frontend/src/InstructorDashboardComponents/CreateCourseView.jsx
@@ -28,8 +28,10 @@ const handleSubmit = async (e) => {
     status: "draft", // or optional
   };
 
+  //  http://localhost:5000   https://course-marketplace-backend.onrender.com
+
   try {
-    const response = await fetch("http://localhost:5000/api/courses", {
+    const response = await fetch("https://course-marketplace-backend.onrender.com/api/courses", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
